262769b11819a549df85966becd4a2508eb37ed0
Mancaceresuade <mancaceres@uade.edu.ar>
2025-01-27 20:10:28 -0300

    actividad 2

---
+++ clase1/ejemplo1/src/Actividad2.java
@@ -0,0 +1,14 @@
+public class Actividad2 {
+    public static void main(String[] args) {
+        int edad = 30;
+        double altura = 1.8;
+        char inicial = 'A';
+        String ciudad = "Los Angeles";
+        int edadAumentada = edad + 10;
+        double alturaPorDos = altura * 2.0;
+        String mensaje = "Tu inicial es " + inicial;
+        System.out.println(edad);
+        System.out.println(alturaPorDos);
+        System.out.println(mensaje);
+    }
+}
