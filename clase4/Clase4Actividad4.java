ff9b0e7486d42dea4654aed972a05850ea499318
Mancaceresuade <mancaceres@uade.edu.ar>
2025-02-03 19:29:01 -0300

    clase 4 act 4 para testear

--- clase4/Clase4Actividad4.java
+++ clase4/Clase4Actividad4.java
@@ -1,6 +1,8 @@
 package clase4;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Clase4Actividad4 {
     public static void main(String[] args) {
@@ -10,13 +12,47 @@ public class Clase4Actividad4 {
 
     private static ArrayList<Integer> obtenerMayores(ArrayList<Integer> numeros, int cantidad) {
         return obtenerMayores(numeros, cantidad, 0, numeros.size());
-    }    
+    }
+
     private static ArrayList<Integer> obtenerMayores(ArrayList<Integer> numeros, int cantidad, int i, int f) {
-        // caso base
-        // mitad
-        // izq
-        // der
-        // combinar
+        ArrayList<Integer> resultado = casoBase(numeros, cantidad, i, f);
+        if (resultado != null) {
+            return resultado;
+        }
+        int mitad = (f + i) / 2;
+        ArrayList<Integer> izq = obtenerMayores(numeros, cantidad, i, mitad);
+        ArrayList<Integer> der = obtenerMayores(numeros, cantidad, mitad, f);
+        return combinar(izq, der);
+    }
+
+    private static ArrayList<Integer> combinar(ArrayList<Integer> izq, ArrayList<Integer> der) {
+        PriorityQueue<Integer> resultado = new PriorityQueue<>(Collections.reverseOrder());
+        for (Integer integer : izq) {
+            resultado.offer(integer);
+        }
+        for( Integer integer : der) {
+            resultado.offer(integer);
+        }
+        return new ArrayList<>(resultado);
+    }
+
+    private static ArrayList<Integer> casoBase(ArrayList<Integer> numeros, int cantidad, int i, int f) {
+        // vacio
+        if (numeros == null || i <= f) {
+            return new ArrayList<>();
+        }
+        // un elemento
+        if (f - i == 1) {
+            ArrayList<Integer> res = new ArrayList<>();
+            res.add(numeros.get(1));
+            return res;
+        }
+        // menor o igual a la cantidad
+        if (f - i <= cantidad) {
+            ArrayList<Integer> resultado = new ArrayList<>(numeros.subList(i, f));
+            Collections.sort(resultado, Collections.reverseOrder());
+            return resultado;
+        }
         return null;
     }
 
